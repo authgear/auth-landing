678a523847cd3399d345183c832845aef91d00ba
Arthelon <hsing.daniel@gmail.com>
2020-06-01 12:14:17 +0800

    Add NotFound page text
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/components/NotFound.svelte
+++ src/components/NotFound.svelte
@@ -1,1 +1,28 @@
-<h1>Something went wrong</h1>
+<style>
+  .notfound {
+    padding: 180px 18px;
+    max-width: 1200px;
+  }
+
+  .notfound__title {
+    margin-bottom: 48px;
+    margin-top: 0;
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 1.4;
+  }
+
+  .notfound__message {
+    font-size: 15px;
+    line-height: 28px;
+    margin-bottom: 16px;
+    margin-top: 0;
+  }
+</style>
+
+<div class="notfound">
+  <h1 class="notfound__title">Something went wrong</h1>
+  <p class="notfound__message">
+    Oops the page you were looking for was not found.
+  </p>
+</div>
